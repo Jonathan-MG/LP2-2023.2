c988361f7dca6fd546ff7295e1278169585152bc
JONATHAN MARTINS GOMES <20.00862-7@maua.br>
2023-08-19 11:08:54 -0300

    Dart - Números aleatórios

---
+++ Aula 14 - Flutter/introduction/bin/introduction.dart
@@ -0,0 +1,91 @@
+// enum DiaDaSemana{
+//     segunda,
+//     terca,
+//     quarta,
+//     quinta,
+//     sexta,
+//     sabado,
+//     domingo
+//   }
+
+// import 'dart:io';
+// import 'dart:math';
+void main(){
+  // final gerador = Random();
+  // final n1 = gerador.nextInt(6); //[0, 6[
+  // final n2 = gerador.nextInt(10) + 1; //[1,11[
+  // final n3 = gerador.nextDouble(); //[0, 1[
+  // final n4 = gerador.nextDouble() * 100; //[0, 100[
+  // final n5 = gerador.nextDouble() * 50 + 100; //[100, 150[
+  // // Int [17,22]
+  //   final n1 = gerador.nextInt(6) + 17; 
+  // // Real [2500,3500[ 
+  //   final n2 = gerador.nextDouble() * 1000 + 2500;
+  //   print(n1);
+  //   print(n2);
+  // final vaiChover = gerador.nextBool();
+
+  // var nome = stdin.readLineSync();
+  // print(nome);
+  // var entrada = stdin.readLineSync();
+  // if (entrada != null){
+  //   int idade = int.parse(entrada);
+  //   print(idade);
+  // }
+  // int idade = int.parse(stdin.readLineSync()!);
+  // print(idade);
+  
+  // print(DiaDaSemana.values);
+  // DiaDaSemana dia = DiaDaSemana.quarta;
+  // print(dia.index);
+  // print(dia.name);
+  // print(dia.runtimeType);
+  // print(dia.toString());
+  // switch(dia){
+  //   case DiaDaSemana.segunda:
+  //     print('NÃÃÃÃÃOOO');
+  //     break;
+  //   case DiaDaSemana.terca:
+  //     print(':(');
+  //     break;
+  //   case DiaDaSemana.quarta:
+  //     print(':(');
+  //     break;
+  //   case DiaDaSemana.quinta:
+  //     print(':(');
+  //     break;
+  //   case DiaDaSemana.sexta:
+  //     print(':D');
+  //     break;
+  //   case DiaDaSemana.sabado:
+  //     print(':DD');
+  //     break;
+  //   case DiaDaSemana.domingo:
+  //     print(':D');
+  //     break;
+  // }
+
+  // for(int e=0;e<10;e++){
+  //   print(e);
+  // }
+  // //do-while
+  // int contador = 0;
+  // do{
+  //   print(contador++);
+  // }while(contador < 10);
+  // //while
+  // int i = 0;
+  // while (i<10){
+  //   print(i);
+  //   i++;
+  // }
+  // //var
+  // var nomes = ['Ana', 'Pedro'];
+  // for (String nome in nomes){
+  //   print(nome);
+  // }
+  // //loop infinito
+  // // for ( ; ; );
+  // print('Hello, World');
+  
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LP2-2023.2
 Repositório para projetos desenvolvidos durante o segundo semestre de 2023.
+
+- dart create introduction -t console-simple
+- Instalar o flutter no vscode
