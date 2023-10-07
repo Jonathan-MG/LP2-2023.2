f0ef26f1a356c6eac44c88bd2670c3900f13bbfc
JONATHAN MARTINS GOMES <20.00862-7@maua.br>
2023-10-07 12:56:44 -0300

    Fim da aula

--- Aula 17/responsividade/lib/main.dart
+++ Aula 17/responsividade/lib/main.dart
@@ -19,8 +19,72 @@ class MeuLayoutResponsivo extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("App responsiva")),
       body: LayoutBuilder(builder: (context, constraints) {
-        return Text(constraints.maxWidth <= 576 ? 'Pequena' : 'Grande');
+        return constraints.maxWidth <= 576 ? MobileLayout() : WebLayout();
       }),
+    );
+  }
+}
+
+class MobileLayout extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MobileLayoutState();
+  }
+}
+
+class MobileLayoutState extends State<MobileLayout>
+    with SingleTickerProviderStateMixin {
+  TabController? tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext) {
+    return Column(
+      children: [
+        TabBar(
+            labelColor: Colors.black,
+            controller: tabController,
+            tabs: [Tab(text: "Aba 1"), Tab(text: "Aba 2")]),
+        Expanded(
+            child: TabBarView(
+          controller: tabController,
+          children: [
+            Center(child: Text("Esse é o conteúdo da primeira aba.")),
+            Center(child: Text("Esse é o conteúdo da segunda aba."))
+          ],
+        ))
+      ],
+    );
+  }
+}
+
+class WebLayout extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return WebLayoutState();
+  }
+}
+
+class WebLayoutState extends State<WebLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+            child: Card(
+                child:
+                    Center(child: Text("Esse é o conteúdo da primeira aba.")))),
+        Expanded(
+            child: Card(
+                child:
+                    Center(child: Text("Esse é o conteúdo da segunda aba."))))
+      ],
     );
   }
 }
