b211689e4f846e06825c0752f4e64de0e3e3ada8
JONATHAN MARTINS GOMES <20.00862-7@maua.br>
2023-10-07 10:51:49 -0300

    Setup para inicio de aula

---
+++ Aula 17/exibe_imagens/lib/src/app.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<App> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int numeroImagens = 0;
+  List<ImageModel> imagens = [];
+
+  void obterImagem() async {
+    var url = Uri.https(
+      'api.pexels.com',
+      '/v1/search',
+      {'query': 'people', 'page': '1', 'per_page': '1'},
+    );
+    var req = http.Request('get', url);
+    req.headers.addAll(
+      {
+        'Authorization': 'ZQNyDNVADHauGkAx99SsggjoC35kATx8tOVJNH1yqcXgNssoB8NAcv8Z',
+      },
+    );
+    final result = await req.send();
+    if (result.statusCode == 200) {
+      final response = await http.Response.fromStream(result);
+      var decodedJSON = json.decode(response.body);
+      var imagem = ImageModel.fromJSON(decodedJSON);
+      setState(() {
+        imagens.add(imagem);
+      });
+    } else {
+      print('Falhou');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Minhas Imagens"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: obterImagem,
+      ),
+      body: ImageList(imagens),
+    ));
+  }
+}
---
+++ Aula 17/exibe_imagens/lib/src/widgets/image_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../models/image_model.dart';
+
+class ImageList extends StatelessWidget {
+  final List<ImageModel> imagens;
+
+  ImageList(this.imagens);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: imagens.length,
+      itemBuilder: (BuildContext context, int index) {
+        // Exibir o alt, caso o elemento esteja em uma posição par e exibir a url caso contrário, use o operador ternário para decidir
+        return Text(index.isEven ? imagens[index].alt : imagens[index].url);
+      },
+    );
+  }
+}
